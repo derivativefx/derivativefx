8f2918da31c0a6388cfdec8cc0a813637428bae1
luxo <luxo@b7eaa87d-ddef-44b2-965b-d739c1fb766f>
2008-06-01 18:36:42 +0000

    split code/language js: change from .js to .js.php because of language translated into german

--- derivativeFX/image.php
+++ derivativeFX/image.php
@@ -1,4 +1,26 @@
 <?php
+/*
+Copyright Luxo 2008
+
+This file is part of derivativeFX.
+
+    derivativeFX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    derivativeFX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with derivativeFX.  If not, see <http://www.gnu.org/licenses/>.
+    
+    */
+$language = $_GET['lang'];
+include("language.php");
+
 //********************* DATENBANK
 include("/home/luxo/public_html/contributions/logindata.php");//pw & bn einbinden
 $dblink = @mysql_connect($databankname, $userloginname, $databasepw);//Allgemein (TS-Database)
@@ -20,7 +42,10 @@ imageSaveAlpha($img, true);
 $font = "/home/luxo/public_html/derivativeFX/font.ttf";
 
 $red = imagecolorallocate($img,225,0,0);
-imageTTFtext($img,15,14,10,85,$red,$font,"$anz images uploaded!");
+
+$spruch = sprintf($lng['x']['imgu'],$anz);
+
+imageTTFtext($img,15,14,10,85,$red,$font,$spruch);
 
 imagepng($img);
 
---
+++ derivativeFX/language.php
@@ -0,0 +1,160 @@
+<?php
+/*
+Copyright Luxo 2008
+
+This file is part of derivativeFX.
+
+    derivativeFX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    derivativeFX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with derivativeFX.  If not, see <http://www.gnu.org/licenses/>.
+    
+    */
+
+//Language file of derivativeFX
+//############################# ENGLISH ########################
+$lng['en']['name'] = "English";
+$lng['en']['welc'] = "Welcome to derivativeFX!";
+$lng['en']['conf'] = "Please confirm that you are logged in to Wikimedia:Commons.";
+$lng['en']['look'] = "If you see your userpage in this frame, you are logged in. If not, please";
+$lng['en']['loin'] = "log in";
+$lng['en']['skip'] = "Skip this check next time";
+$lng['en']['okin'] = "Ok - I'm logged in.";
+$lng['en']['load'] = "Loading";
+$lng['en']['wait'] = "Please wait";
+$lng['en']['prof'] = "preview of";
+$lng['en']['orwo'] = "Original Work";
+$lng['en']['lotf'] = "License of this file";
+$lng['en']['pan']  = "please add name";
+$lng['en']['admo'] = "add more original files";
+$lng['en']['next'] = "OK - Next";
+$lng['en']['skco'] = "Please enable cookies to skip the login check in the next time.";
+$lng['en']['reqdel'] = "This image has been requested for deletion!";
+$lng['en']['notex'] = "File doesn't exist!";
+$lng['en']['orig'] = "Original";
+//***** Form1 fertig, Form2
+$lng['en']['nomerg'] = "<h1>Sorry,</h1> your images can't be merged!<br /><br />The <b>licenses are incompatible</b> or the <b>licenses can't be detected</b>.<br /> See above for all license tags the tool found for this images:";
+$lng['en']['unktem'] = "Unknown templates";
+$lng['en']['unlimg'] = "no known license found for this image!";
+$lng['en']['descri'] = "Description";
+$lng['en']['forpar'] = "for the <code>|description=</code> -parameter in {{Information}}";
+$lng['en']['addtem'] = "Add Template %s to the description";
+$lng['en']['change'] = "Changes";
+$lng['en']['editor'] = "Editor";
+$lng['en']['edihin'] = 'Without wikisyntax, e.g. "Test" for User:Test';
+$lng['en']['others'] = "Other Templates";
+$lng['en']['categs'] = "Categories";
+$lng['en']['nocats'] = "No categories found";
+$lng['en']['licens'] = "License";
+$lng['en']['hincan'] = "You can change the whole summary on the next page";
+//**********Form2 fertig, Form3
+$lng['en']['selfil'] = "Select your derivative file";
+$lng['en']['destin'] = "Destination filename";
+$lng['en']['propos'] = "propositions";
+$lng['en']['desexi'] = "Destination filename already exist. Do you want overwrite existing file?";
+$lng['en']['desoke'] = "Destination filename doesn't exist.";
+$lng['en']['summar'] = "Summary";
+$lng['en']['watcht'] = "Watch this page";
+$lng['en']['addnot'] = "Add a notice to the original file(s) about this derivative work. (by 'User:Bilderbot', at 02:00 UTC)";
+$lng['en']['accept'] = "You confirm that all details in&nbsp;the file description above are
+  <span style=\"font-weight: bold;\">correct and conformable
+with the license(s) of the original-file(s). Images could contain copyrighted paintings and the original-license does not extend to derivatives that isolate them.</span> This tool is not able to check for such copyrighted things. derivativeFX and
+his developer&nbsp;assume no accountability about the correctness
+of the generated content. &nbsp;It has been created in the hope
+that it will be useful, but <span style=\"font-weight: bold;\">WITHOUT
+ANY WARRANTY. You are accountable for
+the correctness! </span>Image name, original files and time is logged.";
+$lng['en']['start'] = "Upload file";
+$lng['en']['attac'] = "Please attach a file.";
+$lng['en']['corfi'] = "No correct file extension found in destination file. (%s)";
+$lng['en']['notsa'] = "Your selected file has not the same file extension (%s) like the destination name (%s)! Change dest. name to '%s'?";
+$lng['en']['imgu'] = "%s images uploaded!";
+
+
+//############################# DEUTSCH ########################
+$lng['de']['name'] = "Deutsch";
+$lng['de']['welc'] = "Willkommen!";
+$lng['de']['conf'] = "Bitte überprüfe ob du in Wikimedia:Commons angemeldet bist.";
+$lng['de']['look'] = "Wenn du deine Benutzerseite in diesem Frame siehst, bist du angemeldet. Wenn nicht, bitte";
+$lng['de']['loin'] = "Anmelden";
+$lng['de']['skip'] = "Diese Überprüfung nicht mehr anzeigen";
+$lng['de']['okin'] = "Ok - Ich bin angemeldet.";
+$lng['de']['load'] = "Laden";
+$lng['de']['wait'] = "Bitte warten";
+$lng['de']['prof'] = "Vorschau von";
+$lng['de']['orwo'] = "Originaldatei";
+$lng['de']['lotf'] = "Lizenz dieser Datei";
+$lng['de']['pan']  = "gib einen Namen an.";
+$lng['de']['admo'] = "Mehr Originale angeben";
+$lng['de']['next'] = "Ok - Weiter";
+$lng['de']['skco'] = "Bitte aktiviere Cookies um die Überprüfung nicht mehr anzuzeigen.";
+$lng['de']['reqdel'] = "Diese Datei wurde zum Löschen vorgeschlagen!";
+$lng['de']['notex'] = "Datei existiert nicht!";
+$lng['de']['orig'] = "Original";
+//***** Form1 fertig, Form2
+$lng['de']['nomerg'] = "<h1>Sorry,</h1> deine Bilder können nicht zusammengeführt werden!<br /><br />The <b>Die Lizenzen sind inkompatibel</b> oder die <b>Lizenzen können nicht gefunden werden</b>.<br /> Das Tool hat folgende Lizenzen gefunden:";
+$lng['de']['unktem'] = "Unbekannte Vorlagen";
+$lng['de']['unlimg'] = "Keine bekannte Lizenz gefunden!";
+$lng['de']['descri'] = "Beschreibung";
+$lng['de']['forpar'] = "für den <code>|description=</code> -Parameter in {{Information}}";
+$lng['de']['addtem'] = "Vorlage %s einfügen";
+$lng['de']['change'] = "Änderungen";
+$lng['de']['editor'] = "Editor";
+$lng['de']['edihin'] = 'Ohne Wikisyntax, z. B. "Test" für Benutzer:Test';
+$lng['de']['others'] = "Andere Vorlagen";
+$lng['de']['categs'] = "Kategorien";
+$lng['de']['nocats'] = "Keine Kategorien gefunden";
+$lng['de']['licens'] = "Lizenz";
+$lng['de']['hincan'] = "Du kannst die ganze Beschreibung auf der nächsten Seite bearbeiten";
+//**********Form2 fertig, Form3
+$lng['de']['selfil'] = "Deine bearbeitete Datei";
+$lng['de']['destin'] = "Zielname";
+$lng['de']['propos'] = "Vorschläge";
+$lng['de']['desexi'] = "Zielname existiert bereits. Willst du die bestehende Datei überschreiben?";
+$lng['de']['desoke'] = "Zielname existiert noch nicht.";
+$lng['de']['summar'] = "Beschreibung/Quelle";
+$lng['de']['watcht'] = "Datei beobachten";
+$lng['de']['addnot'] = "Bei den Originalen einen Hinweis über diese Bearbeitung eintragen. (durch 'User:Bilderbot' am 02:00 UTC)";
+$lng['de']['accept'] = "Du bestätigst dass alle Details in der Dateibeschreibung
+  <span style=\"font-weight: bold;\">korrekt und kompatibel mit den Originaldateien sind. Dateien können geschützte Teile enthalten. Die Originallizenz gilt nicht für Bearbeitungen, welche diese freistellen.</span> Dieses Tool ist nicht in der Lage, solche geschützten Teile zu finden. derivativeFX  und sein Entwickler
+ übernehmen keine Verantwortung für den generierten Inhalt. Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen von Nutzen sein wird, aber <span style=\"font-weight: bold;\">OHNE IRGENDEINE GARANTIE.
+ Du bist verantwortlich für die Richtigkeit der Angaben! </span>Zielname, Originaldateien und Zeit wird aufgezeichent.";
+$lng['de']['start'] = "Datei Hochladen";
+$lng['de']['attac'] = "Bitte gib eine Datei an.";
+$lng['de']['corfi'] = "Keine korrekte Dateiendung gefunden im Dateiname. (%s)";
+$lng['de']['notsa'] = "Deine ausgewählte Datei hat nicht die selbe Dateiendung (%s) wie der Zielname (%s)! Zielname ändern zu '%s'?";
+$lng['de']['imgu'] = "%s Dateien hochgeladen!";
+
+
+
+
+
+
+
+
+
+//##############################################################################
+
+if(array_key_exists($language,$lng))
+{
+$sxlang = $language;
+}
+else
+{
+$sxlang = "en";
+$language = "en";
+}
+
+$lng['x'] = $lng[$sxlang];
+
+
+
+?>
