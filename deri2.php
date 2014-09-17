c4817d7f38d92c48dc07dee36d3b6b08be4d3e32
Steinsplitter <steinsplitter-wiki@live.com>
2014-09-17 12:20:27 +0200

    fix footer

--- deri2.php
+++ deri2.php
@@ -510,17 +510,9 @@ if ( $isaccord == false ) {
 }
 ?>
 	<hr style="height: 2px; width: 60%;">
-<div style="text-align: center;">by <a href="/%7Eluxo/">Luxo</a>
-	| <a href="http://commons.wikimedia.org/wiki/User_talk:Luxo">contact</a>
-	| <a
-		href="http://meta.wikimedia.org/wiki/User:Luxo/Licenses#derivativeFX">license</a><br/>
-	<br/>
-	<a href="http://wiki.ts.wikimedia.org/view/Main_Page"><img
-			style="border: 0px solid ; width: 88px; height: 31px;"
-			alt="powered by Wikimedia Toolserver"
-			title="powered by Wikimedia Toolserver"
-			src="http://tools.wikimedia.de/images/wikimedia-toolserver-button.png"></a>&nbsp;</div>
-
+<div style="text-align: center;">The tool was originally written by <a href="//commons.wikimedia.org/wiki/User:Luxo">Luxo</a>
+	| <a href="">about</a>
+	| <a href="//github.com/derivativefx/derivativefx">source & license</a><br/>
 </span>
 </body>
 </html>
